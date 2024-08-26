3e41690b8684ebd159e3df2196fc6cc359dc8431
Gonzalo Ventura <gonzalogventura@gmail.com>
2024-08-26 19:17:52 -0300

    update

--- app/page.js
+++ app/page.js
@@ -19,17 +19,17 @@ export default function Home() {
             src={logo}
             className="animate-jump-in animate-once animate-delay-500 animate-duration-[3500ms] mt-24"
           /> */}
-          <div className="flex justify-end mt-[30vh]">
+          <div className="flex justify-end mt-[35vh]">
             <Link
               href={"/game"}
-              className="animate-fade animate-once animate-delay-1000 transition-all duration-500 border border-gray-500 bg-gray-800 bg-opacity-50 text-white rounded-full shadow px-16 py-4 scale-125 focus:shadow focus:shadow-gray-500"
+              className="animate-fade animate-once animate-delay-1000 transition-all duration-500 border border-gray-500 bg-gray-800 bg-opacity-50 text-white rounded-full shadow px-16 py-4 focus:shadow focus:shadow-gray-500"
             >
               Comenzar
             </Link>
           </div>
         </div>
         <video muted autoPlay loop className='video'>
-          <source src="./fondo.mp4" type="video/mp4" />
+          <source src="./allvideos_1.mp4" type="video/mp4" />
         </video>
       </main>
     </>
